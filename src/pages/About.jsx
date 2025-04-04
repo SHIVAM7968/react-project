a8d107c35105e7b86d577de093770ba6c609cd8e
shivam khalasi <shivamkhalasi55@gmail.com>
2025-04-04 11:53:15 +0530

    Your commit message

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,27 +4,30 @@ import logo1 from '../assets/apple.png';
 import logo2 from '../assets/microsoft.png';
 import logo3 from '../assets/slack.png';
 import logo4 from '../assets/google.png';
+
 function About() {
   return (
     <>
+   
     <div className='bg-blue-800 md:w-full text-center md:p-20'>
       <h2 className='font-bold  text-white text-5xl'>Your work, everywhere you are</h2>
       <br/>
       <p className=' text-white '>Access your notes from your computer, phone or tablet by synchronising with various services, including whitepace, Dropbox and OneDrive. The app is available on Windows, macOS, Linux, Android and iOS. A terminal app is also available!</p>
      <br/>
-     <div className='md:flex justify-center'>
+     <div className='flex justify-center'>
       <button className='px-6 py-4 text-white  bg-blue-400 rounded-md flex text-center'>Try Taskey<FaArrowRightLong className='m-1' /></button>
      </div>
     </div>
-    <div className='bg-white md:p-20 md:w-full text-center'>
-    <h2 className='font-bold text-5xl'>Our sponsors</h2>
-    <div className='md:flex w-full  gap-40 justify-center md:pt-10'>
-       <img src={logo1} alt='{logo1}' className='size-28'/>
+    <div className='bg-white md:p-20 w-full  text-center md:justify-center'>
+    <h2 className='font-bold text-5xl shadow-md shadow-yellow-400 rounded-md '>Our sponsors</h2>
+    <div className='md:flex md:w-full  md:gap-40 md:justify-center md:pt-10 '>
+       <img src={logo1} alt='{logo1}' className='size-28 md:items-center'/>
        <img src={logo2} alt='{logo2}'className='size-28'/>
        <img src={logo3} alt='{logo3}'className='size-28'/>
        <img src={logo4} alt='{logo4}'className='size-28'/>
     </div>
     </div>
+    
     </>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
-
+import { FaQuoteLeft } from "react-icons/fa";
+import logo from '../assets/clientimg.jpg'
+import logo1 from '../assets/clientimg1.jpg'
+import logo2 from '../assets/clientimg2.jpg'
 function Contact() {
   return (
-    <div className='bg-yellow-600'>Contact</div>
+    <>
+    <div className='w-full md:flex md:justify-center md:pt-10'>
+         <h2 className='text-5xl font-bold shadow-md shadow-yellow-300 rounded-md'>What Our Clients Says</h2>
+    </div>
+   <div className='w-full md:flex bg-white md:p-20 md:gap-8 '>
+     <div className="lg:pt-2 pt-7 rounded-md  border-none md:w-1/3 p-5 shadow-md shadow-black-100  ">
+        <div className=''>
+        <FaQuoteLeft className='size-14 fill-blue-950' />
+        </div>
+        <p className='md:mt-5 '>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>          
+        <hr className='border-4 md:mt-5'></hr>
+        <div className="md:overflow-hidden md:rounded-full w-20 h-20 md:mt-5">
+                   <img src={logo} alt='{logo}' className='w-20 h-20 '/>           
+         </div>
+         <div className=''>
+         <h2 className='font-bold text-1xl '>Oberon Shaw, MCH</h2>
+         <p className=''>Head of Talent Acquisition, North America</p>
+         </div>
+      </div>
+      <div className="lg:pt-2 pt-7 rounded-md bg-blue-400 border-none md:w-1/3 p-5 shadow-black-100 shadow-md">
+        <div className=''>
+        <FaQuoteLeft className='size-14 fill-white' />
+        </div>
+        <p className='md:mt-5 text-white'>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>          
+        <hr className='border-4 md:mt-5'></hr>
+        <div className="md:overflow-hidden md:rounded-full w-20 h-20    md:mt-5">
+                   <img src={logo1} alt='{logo}' className='w-20 h-20 '/>           
+         </div>
+         <div className=''>
+         <h2 className='font-bold text-1xl text-blue-950'>Oberon Shaw, MCH</h2>
+         <p className='text-white'>Head of Talent Acquisition, North America</p>
+         </div>
+      </div>
+      <div className="lg:pt-2 pt-7 rounded-md bg-blue-400 border-none md:w-1/3 p-5 shadow-black-100 shadow-md ">
+        <div className=''>
+        <FaQuoteLeft className='size-14 fill-white' />
+        </div>
+        <p className='md:mt-5 text-white'>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>          
+        <hr className='border-4 md:mt-5'></hr>
+        <div className="md:overflow-hidden md:rounded-full w-20 h-20 md:mt-5">
+                   <img src={logo2} alt='{logo}' className='w-20 h-20 '/>           
+         </div>
+         <div className=''>
+         <h2 className='font-bold text-1xl text-blue-950'>Oberon Shaw, MCH</h2>
+         <p className='text-white'>Head of Talent Acquisition, North America</p>
+         </div>
+      </div>
+   </div>
+    </>
   )
 }
 
